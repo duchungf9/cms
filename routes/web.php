deaf2cfa5fb51e23be4dcda186d1965d46edc16a
duchungf9 <duchungf9@gmail.com>
2023-12-04 11:33:43 +0700

    test 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::any('/media/upload', function(){
+    return response()->json(['error' => 0]);
+});
